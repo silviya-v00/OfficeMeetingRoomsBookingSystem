f873a786e4725b9988f4bbddd6f4e9df4b360475
silviya-v00 <sisi.vasileva2709@gmail.com>
2024-05-12 14:07:40 +0300

    OMRB-4: Add ProfileImages table for the images structure in user profile functionality

---
+++ OfficeMeetingRoomsBookingSystem/OfficeMeetingRoomsBookingSystem/Models/ProfileImages.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeMeetingRoomsBookingSystem.Models
+{
+    public class ProfileImages
+    {
+        public int ProfileImageID { get; set; }
+        public string UserID { get; set; }
+        public string ProfileImageFileName { get; set; }
+        public IFormFile ProfileImage { get; set; }
+    }
+}
